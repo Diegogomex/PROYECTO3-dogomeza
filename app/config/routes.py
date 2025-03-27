bbf2bded0adb510fa3d3eb045a86170995e96ec1
Diegogomex <diegogo1320@gmail.com>
2025-03-26 22:13:14 -0500

    Despliegue Proyecto modulo 4(correccion en auth/login)

--- app/config/routes.py
+++ app/config/routes.py
@@ -3,9 +3,10 @@ from app.controllers.api_controller import api_bp
 from app.config.auth_routes import auth_bp
 
 def register_routes(app):
-   
+      
+    app.register_blueprint(auth_bp)     
     app.register_blueprint(home_blueprint)
     app.register_blueprint(api_bp)         
-    app.register_blueprint(auth_bp)         
+        
 
 
