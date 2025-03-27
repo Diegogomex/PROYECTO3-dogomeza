4dd1df4f4a0d9268a909b02af065c6fc2adc5233
Diegogomex <diegogo1320@gmail.com>
2025-03-26 22:53:43 -0500

    Despliegue Proyecto modulo 4(correccion en auth/login)

--- app/config/auth_routes.py
+++ app/config/auth_routes.py
@@ -4,6 +4,10 @@ from flask_login import login_user, logout_user
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
+@auth_bp.route("/test", methods=["POST"])
+def test():
+    return jsonify({"mensaje": "Prueba exitosa"}), 200
+
 @auth_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
